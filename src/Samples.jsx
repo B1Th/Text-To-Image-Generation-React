f12a60cbae08430e5e99b1757ff30b31cdae448f
Bhuwan Thapa <thapabhuwan15@gmail.com>
2023-06-06 23:47:52 +0545

    added overlay in sample images

--- src/Samples.jsx
+++ src/Samples.jsx
@@ -9,25 +9,73 @@ import img5 from "../src/images/astronaut_in_space.png";
 import img6 from "../src/images/man_with_umbrella.jpg";
 import img7 from "../src/images/dog_playing_with_a_ball.jpg";
 import img8 from "../src/images/starry_night_sky_of_kathmandu.jpg";
-import img9 from "../src/images/a_sunset_above_sea_near_himalayas.jpg";
+import img9 from "../src/images/a_realistic_image_of_humans_fighting_with_alien_spaceship.jpg";
 import img10 from "../src/images/3d_render_of_a_golden_buggati_car.jpg";
 import img11 from "../src/images/teddy_bear_on_a_skateboard_in_Kathmandu.jpg";
 import img12 from "../src/images/3d_image_of_helicopter_shooting_a_yeti_in_the_mountain.jpg";
 
 const Samples = () => {
-  const images = [
-    img1,
-    img2,
-    img3,
-    img4,
-    img5,
-    img6,
-    img7,
-    img8,
-    img9,
-    img10,
-    img11,
-    img12,
+  const imagesData = [
+    {
+      id: 1,
+      imagesrc: img1,
+      title: "city of thousand balloons",
+    },
+    {
+      id: 2,
+      imagesrc: img2,
+      title: "dog and little boy playing in the park ",
+    },
+    {
+      id: 3,
+      imagesrc: img3,
+      title: "man and woman climbling mountian on a sheep",
+    },
+    {
+      id: 4,
+      imagesrc: img4,
+      title: "a city with rainy cloud",
+    },
+    {
+      id: 5,
+      imagesrc: img5,
+      title: "astronaut in space",
+    },
+    {
+      id: 6,
+      imagesrc: img6,
+      title: "man with umbrella",
+    },
+    {
+      id: 7,
+      imagesrc: img7,
+      title: "dog playing with a ball",
+    },
+    {
+      id: 8,
+      imagesrc: img8,
+      title: "starry night sky of kathmandu",
+    },
+    {
+      id: 9,
+      imagesrc: img9,
+      title: "a realistic image of human fighting against alien in spaceship",
+    },
+    {
+      id: 10,
+      imagesrc: img10,
+      title: "3d render of a golden buggati car",
+    },
+    {
+      id: 11,
+      imagesrc: img11,
+      title: "tedy bear on a skateboard in kathmandu",
+    },
+    {
+      id: 12,
+      imagesrc: img12,
+      title: "3d image of helicopter shooting a yeti in the mountain",
+    },
   ];
 
   useEffect(() => {
@@ -42,9 +90,12 @@ const Samples = () => {
             Here are some <span className="gradient-text"> Samples</span>
           </h1>
           <div className="sample_container" data-aos="fade-right">
-            {images.map((image, index) => (
-              <div className="sample_item" key={index}>
-                <img src={image} alt="samples" />
+            {imagesData.map((image) => (
+              <div className="sample_item" key={image.id}>
+                <img src={image.imagesrc} alt="samples" />
+                <div className="overlay">
+                  <h3>{image.title}</h3>
+                </div>
               </div>
             ))}
           </div>

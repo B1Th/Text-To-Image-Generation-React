0f58c0731f4b4a0aa2c7d9be3366c83a5cf38a52
Bhuwan Thapa <thapabhuwan15@gmail.com>
2023-06-06 21:06:12 +0545

    Fixed some bugs

--- backend/api/views.py
+++ backend/api/views.py
@@ -14,39 +14,44 @@ from django.conf import settings
 
 
 class CFG:
-    seed = 42
-    device = torch.device("cuda")
-    generator = torch.Generator(device=device).manual_seed(seed)
-    image_gen_steps = 50
-    image_gen_model_id = "stabilityai/stable-diffusion-2"
-    image_gen_size = (400, 400)
-    image_gen_guidance_scale = 9
-    prompt_gen_model_id = "gpt2"
-    prompt_dataset_size = 6
-    prompt_max_length = 12
+    def __init__(self):
+        self.seed = 42
+        self.device = torch.device("cuda")
+        self.generator = torch.Generator(device=self.device).manual_seed(self.seed)
+        self.image_gen_steps = 50
+        self.image_gen_model_id = "stabilityai/stable-diffusion-2"
+        self.image_gen_size = (400, 400)
+        self.image_gen_guidance_scale = 9
+        self.prompt_gen_model_id = "gpt2"
+        self.prompt_dataset_size = 6
+        self.prompt_max_length = 12
 
 
-# Create the image_gen_model
-    image_gen_model = StableDiffusionPipeline.from_pretrained(
-        image_gen_model_id,
-        torch_dtype=torch.float16,
-        revision="fp16",
-        use_auth_token='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',
-        guidance_scale=image_gen_guidance_scale
+# Create an instance of the CFG class
+config = CFG()
+
+
+# Create the image_gen_model using the config object
+image_gen_model = StableDiffusionPipeline.from_pretrained(
+    config.image_gen_model_id,
+    torch_dtype=torch.float16,
+    revision="fp16",
+    use_auth_token='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',
+    guidance_scale=config.image_gen_guidance_scale, generator=config.generator
     )
-    image_gen_model = image_gen_model.to(device)
+image_gen_model = image_gen_model.to(config.device)
 
 
 def generate_image(prompt, model):
     image = model(
         prompt,
-        num_inference_steps=CFG.image_gen_steps,
-        generator=CFG.generator,
-        guidance_scale=CFG.image_gen_guidance_scale
+        num_inference_steps=config.image_gen_steps,
+        generator=config.generator,
+        guidance_scale=config.image_gen_guidance_scale
     ).images[0]
 
 
-    image = image.resize(CFG.image_gen_size)
+    image = image.resize(config.image_gen_size)
     #save the image
     filename = f"{prompt.replace(' ','_')}.jpg"
     image_folder_path = os.path.join(settings.MEDIA_ROOT,'generated_images')
@@ -63,7 +68,7 @@ class ImageGenerationSerializer(serializers.Serializer):
 
     def create(self,validated_data):
         text_prompt = validated_data.get('text_prompt')
-        image_path = generate_image(text_prompt,CFG.image_gen_model)
+        image_path = generate_image(text_prompt,image_gen_model)
 
         return {'image':image_path}
     
